c7c90c09710fe1d476fcda3e2b2d9918e0e8f8dd
Luan A. <barkon32@tuta.io>
2024-02-27 11:44:56 -0300

    feat: initial commit

---
+++ src/main/java/com/luan/desafio/desafioestagio/DesafioestagioApplication.java
@@ -0,0 +1,13 @@
+package com.luan.desafio.desafioestagio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioestagioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioestagioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luan/desafio/desafioestagio/DesafioestagioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luan.desafio.desafioestagio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioestagioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
