f22ff980b8ef709947777ecb6a0585bcb6f07e0b
Luan A. <barkon32@tuta.io>
2024-02-28 15:23:31 -0300

    feat: create constructor for sign up dto

--- src/main/java/com/luan/desafio/desafioestagio/model/Cliente.java
+++ src/main/java/com/luan/desafio/desafioestagio/model/Cliente.java
@@ -1,5 +1,6 @@
 package com.luan.desafio.desafioestagio.model;
 
+import com.luan.desafio.desafioestagio.dto.CadastrarClienteDto;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Entity;
@@ -9,10 +10,12 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.GenerationType;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @Table(name = "clientes")
+@NoArgsConstructor
 public class Cliente {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,4 +25,10 @@ public class Cliente {
     private String cpf;
     @NotBlank
     private String email;
+
+    public Cliente(CadastrarClienteDto dto) {
+        this.nome = dto.getNome();
+        this.cpf = dto.getCpf();
+        this.email = dto.getEmail();
+    }
 }
