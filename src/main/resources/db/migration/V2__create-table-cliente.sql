37fac763e926da6a7d028764583dc59637659037
Luan A. <barkon32@tuta.io>
2024-02-28 15:45:03 -0300

    feat: add table cliente columns on table creation

--- src/main/resources/db/migration/V2__create-table-cliente.sql
+++ src/main/resources/db/migration/V2__create-table-cliente.sql
@@ -1,7 +1,14 @@
 CREATE TABLE clientes (
-  id BIGINT AUTO_INCREMENT NOT NULL,
-   nome VARCHAR(255) NOT NULL,
-   cpf VARCHAR(11) NOT NULL,
-   email VARCHAR(255) NOT NULL,
-   CONSTRAINT pk_clientes PRIMARY KEY (id)
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    cpf VARCHAR(11) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    logradouro varchar(100) not null,
+    bairro varchar(100) not null,
+    cep varchar(9) not null,
+    complemento varchar(100),
+    numero varchar(20),
+    uf char(2) not null,
+    cidade varchar(100) not null,
+    CONSTRAINT pk_clientes PRIMARY KEY (id)
 );
