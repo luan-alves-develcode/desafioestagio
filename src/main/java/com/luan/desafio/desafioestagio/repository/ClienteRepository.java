29113d96bdb8b12a73565aacd537a9f73b7ba1f9
Luan A. <barkon32@tuta.io>
2024-02-28 15:50:21 -0300

    feat: correct type of Integer to Long

--- src/main/java/com/luan/desafio/desafioestagio/repository/ClienteRepository.java
+++ src/main/java/com/luan/desafio/desafioestagio/repository/ClienteRepository.java
@@ -3,5 +3,5 @@ package com.luan.desafio.desafioestagio.repository;
 import com.luan.desafio.desafioestagio.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
