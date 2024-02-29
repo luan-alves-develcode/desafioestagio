6f7600c9b46d44e0e8cb87502518ea28dba1007e
Luan A. <barkon32@tuta.io>
2024-02-29 14:50:19 -0300

    feat: create Produto repository

---
+++ src/main/java/com/luan/desafio/desafioestagio/repository/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package com.luan.desafio.desafioestagio.repository;
+
+import com.luan.desafio.desafioestagio.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
