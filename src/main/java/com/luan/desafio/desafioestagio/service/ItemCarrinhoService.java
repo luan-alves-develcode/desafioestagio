af983b504db25361f38c1783d19b37e0cc437000
Luan A. <barkon32@tuta.io>
2024-02-29 15:47:03 -0300

    feat: create action in ItemCarrinhoService

--- src/main/java/com/luan/desafio/desafioestagio/service/ItemCarrinhoService.java
+++ src/main/java/com/luan/desafio/desafioestagio/service/ItemCarrinhoService.java
@@ -1,10 +1,11 @@
 package com.luan.desafio.desafioestagio.service;
 
-import com.luan.desafio.desafioestagio.dto.ItemParaoCarrinhoDto;
 import com.luan.desafio.desafioestagio.model.ItemCarrinho;
-import com.luan.desafio.desafioestagio.model.ItemCarrinhoRepository;
+import com.luan.desafio.desafioestagio.repository.ItemCarrinhoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ItemCarrinhoService {
@@ -14,5 +15,9 @@ public class ItemCarrinhoService {
 
     public void salvar(ItemCarrinho item) {
         itemCarrinhoRepository.save(item);
+    }
+
+    public List<ItemCarrinho> encontrarItensCarrinhoPorCarrinhoId(Long id) {
+        return itemCarrinhoRepository.findAllByCarrinhoId(id);
     }
 }
