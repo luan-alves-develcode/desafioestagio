c75d03ccb9aef4071c25ef296d61d0be3548d41d
Luan A. <barkon32@tuta.io>
2024-02-29 14:49:05 -0300

    feat: add toString, annotations and id initialization to fix entity failing save

--- src/main/java/com/luan/desafio/desafioestagio/model/ItemCarrinho.java
+++ src/main/java/com/luan/desafio/desafioestagio/model/ItemCarrinho.java
@@ -5,15 +5,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "item_carrinho")
 public class ItemCarrinho {
     @EmbeddedId
-    ItemCarrinhoKey id;
+    ItemCarrinhoKey id = new ItemCarrinhoKey();
 
     @ManyToOne
     @MapsId("produtoId")
@@ -32,4 +36,14 @@ public class ItemCarrinho {
         this.carrinho = carrinho;
         this.quantidade = quantidade;
     }
+
+    @Override
+    public String toString() {
+        return "ItemCarrinho{" +
+                "produto=" + produto +
+                ", carrinho_id=" + carrinho.getId() +
+                ", quantidade=" + quantidade +
+                '}';
+    }
 }
+
