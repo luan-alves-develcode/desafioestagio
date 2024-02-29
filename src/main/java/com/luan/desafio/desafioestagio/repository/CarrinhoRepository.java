db5833ce8997a495dc20e1a5f2645e0a2ba3a71f
Luan A. <barkon32@tuta.io>
2024-02-29 14:50:02 -0300

    feat: create implementation to find by Cliente id

--- src/main/java/com/luan/desafio/desafioestagio/repository/CarrinhoRepository.java
+++ src/main/java/com/luan/desafio/desafioestagio/repository/CarrinhoRepository.java
@@ -4,4 +4,5 @@ import com.luan.desafio.desafioestagio.model.Carrinho;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CarrinhoRepository extends JpaRepository<Carrinho, Long> {
+    Carrinho findCarrinhoByClienteId(Long id);
 }
