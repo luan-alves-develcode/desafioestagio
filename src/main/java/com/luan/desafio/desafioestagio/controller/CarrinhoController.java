0389e8408e740d9cbbfc31ce89568513922a744d
Luan A. <barkon32@tuta.io>
2024-02-29 14:55:07 -0300

    feat: create action to save item to cart and added Transactional annotations

--- src/main/java/com/luan/desafio/desafioestagio/controller/CarrinhoController.java
+++ src/main/java/com/luan/desafio/desafioestagio/controller/CarrinhoController.java
@@ -1,10 +1,13 @@
 package com.luan.desafio.desafioestagio.controller;
 
+import com.luan.desafio.desafioestagio.dto.ItemParaoCarrinhoDto;
 import com.luan.desafio.desafioestagio.service.CarrinhoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +19,21 @@ public class CarrinhoController {
     CarrinhoService carrinhoService;
 
     @PostMapping("/{clienteId}")
+    @Transactional
     public ResponseEntity<String> criar(@PathVariable Long clienteId) {
         try {
             carrinhoService.criar(clienteId);
+            return ResponseEntity.ok().build();
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body(exception.getMessage());
+        }
+    }
+
+    @PostMapping("/{clienteId}/adicionar")
+    @Transactional
+    public ResponseEntity<String> adicionarItemAoCarrinho(@RequestBody ItemParaoCarrinhoDto itemParaCarrinhoDto, @PathVariable Long clienteId) {
+        try {
+            carrinhoService.adicionarItemAoCarrinho(itemParaCarrinhoDto, clienteId);
             return ResponseEntity.ok().build();
         } catch (Exception exception) {
             return ResponseEntity.badRequest().body(exception.getMessage());
