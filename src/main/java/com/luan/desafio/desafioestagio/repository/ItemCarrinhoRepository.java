fa80c205bb1b49f8a11040bbcccfa042280c3a99
Luan A. <barkon32@tuta.io>
2024-02-29 15:45:50 -0300

    feat: create action findALlByCarrinhoId

--- src/main/java/com/luan/desafio/desafioestagio/repository/ItemCarrinhoRepository.java
+++ src/main/java/com/luan/desafio/desafioestagio/repository/ItemCarrinhoRepository.java
@@ -3,6 +3,10 @@ package com.luan.desafio.desafioestagio.repository;
 import com.luan.desafio.desafioestagio.model.ItemCarrinho;
 import com.luan.desafio.desafioestagio.model.ItemCarrinhoKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ItemCarrinhoRepository extends JpaRepository<ItemCarrinho, ItemCarrinhoKey> {
+    List<ItemCarrinho> findAllByCarrinhoId(Long id);
 }
