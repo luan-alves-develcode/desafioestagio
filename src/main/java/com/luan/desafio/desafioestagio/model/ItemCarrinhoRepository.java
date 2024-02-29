68ffadf729a4f15d5a9a802cc50ca8c444beba18
Luan A. <barkon32@tuta.io>
2024-02-29 14:51:44 -0300

    feat: create ItemCarrinho repository

---
+++ src/main/java/com/luan/desafio/desafioestagio/model/ItemCarrinhoRepository.java
@@ -0,0 +1,6 @@
+package com.luan.desafio.desafioestagio.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemCarrinhoRepository extends JpaRepository<ItemCarrinho, ItemCarrinhoKey> {
+}
