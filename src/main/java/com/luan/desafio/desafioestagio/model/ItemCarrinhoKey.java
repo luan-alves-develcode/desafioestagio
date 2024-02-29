382a1471fbe095c6c031bbd43d6d626a001e3370
Luan A. <barkon32@tuta.io>
2024-02-29 14:49:25 -0300

    feat: add Lombok annotations

--- src/main/java/com/luan/desafio/desafioestagio/model/ItemCarrinhoKey.java
+++ src/main/java/com/luan/desafio/desafioestagio/model/ItemCarrinhoKey.java
@@ -2,12 +2,18 @@ package com.luan.desafio.desafioestagio.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Embeddable
 @EqualsAndHashCode
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemCarrinhoKey implements Serializable {
     @Column(name = "produto_id")
     private Long produtoId;
