84a0a8d6cd3ec042f2c21d64c6617cb98fcbfeaa
Luan A. <barkon32@tuta.io>
2024-03-01 15:11:12 -0300

    feat: create custom exception for validation

---
+++ src/main/java/com/luan/desafio/desafioestagio/exception/ValidacaoException.java
@@ -0,0 +1,7 @@
+package com.luan.desafio.desafioestagio.exception;
+
+public class ValidacaoException extends RuntimeException{
+    public ValidacaoException(String message) {
+        super(message);
+    }
+}
