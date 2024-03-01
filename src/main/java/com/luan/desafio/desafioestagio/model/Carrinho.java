d8404e19b0f5a3fc3893ebb3122e7a42ed344be5
Luan A. <barkon32@tuta.io>
2024-03-01 11:04:14 -0300

    feat: implement HashMap

--- src/main/java/com/luan/desafio/desafioestagio/model/Carrinho.java
+++ src/main/java/com/luan/desafio/desafioestagio/model/Carrinho.java
@@ -1,14 +1,11 @@
 package com.luan.desafio.desafioestagio.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -17,11 +14,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 @Getter
