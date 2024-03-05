269841fdb4803d7bd8a1a05bd074f7408b1e3fe3
Luan A. <barkon32@tuta.io>
2024-03-05 15:07:41 -0300

    feat: add validation to Produto creation

--- src/main/java/com/luan/desafio/desafioestagio/repository/ProdutoRepository.java
+++ src/main/java/com/luan/desafio/desafioestagio/repository/ProdutoRepository.java
@@ -4,4 +4,5 @@ import com.luan.desafio.desafioestagio.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    boolean existsByNome(String nome);
 }
--- src/main/java/com/luan/desafio/desafioestagio/service/ProdutoService.java
+++ src/main/java/com/luan/desafio/desafioestagio/service/ProdutoService.java
@@ -1,6 +1,7 @@
 package com.luan.desafio.desafioestagio.service;
 
 import com.luan.desafio.desafioestagio.dto.CadastrarProdutoDto;
+import com.luan.desafio.desafioestagio.exception.ValidacaoException;
 import com.luan.desafio.desafioestagio.model.Produto;
 import com.luan.desafio.desafioestagio.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,10 @@ public class ProdutoService {
     }
 
     public Produto salvar(CadastrarProdutoDto dto) {
-        Produto produto = new Produto(dto);
-        return produtoRepository.save(produto);
+        boolean jaCadastrado = produtoRepository.existsByNome(dto.getNome());
+        if (jaCadastrado) {
+            throw new ValidacaoException("Produto com este nome já existe.");
+        }
+        return produtoRepository.save(new Produto(dto));
     }
 }
