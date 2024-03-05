b78e171c015a84ef3ddf5b9a86398265caae7451
Luan A. <barkon32@tuta.io>
2024-03-05 12:24:23 -0300

    fix: add missing Service annotation

--- src/main/java/com/luan/desafio/desafioestagio/service/VendaService.java
+++ src/main/java/com/luan/desafio/desafioestagio/service/VendaService.java
@@ -3,7 +3,9 @@ package com.luan.desafio.desafioestagio.service;
 import com.luan.desafio.desafioestagio.model.Venda;
 import com.luan.desafio.desafioestagio.repository.VendaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class VendaService {
     @Autowired
     VendaRepository vendaRepository;
