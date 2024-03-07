469a48bbde2c7f9a46750b2081aea18283166b88
Luan A. <barkon32@tuta.io>
2024-03-07 11:02:21 -0300

    feat: findCarrinhoByClienteId now returns an optional

--- src/main/java/com/luan/desafio/desafioestagio/repository/CarrinhoRepository.java
+++ src/main/java/com/luan/desafio/desafioestagio/repository/CarrinhoRepository.java
@@ -3,6 +3,8 @@ package com.luan.desafio.desafioestagio.repository;
 import com.luan.desafio.desafioestagio.model.Carrinho;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CarrinhoRepository extends JpaRepository<Carrinho, Long> {
-    Carrinho findCarrinhoByClienteId(Long id);
+    Optional<Carrinho> findCarrinhoByClienteId(Long id);
 }
